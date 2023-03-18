f4810c0328bc7948635df589a57da0fbe373be22
Vaibhav Thukral <vthukral@nvidia.com>
2023-03-18 12:45:55 -0700

    typescript

---
+++ typescript/typescript-testing/hello.js
@@ -0,0 +1,8 @@
+var name1 = 'hello';
+var name2 = 'what';
+console.log(name1);
+console.log(name2);
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5.5));
---
+++ typescript/typescript-testing/hello.ts
@@ -0,0 +1,11 @@
+let name1 = 'hello'
+let name2 = 'what'
+
+console.log(name1);
+console.log(name2);
+
+const circ = (diameter) => {
+    return diameter * Math.PI;
+};
+
+console.log(circ(5.5));
